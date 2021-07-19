bb9ce5c9855b98ecf35fb31542a49d58575c2b1c
Nikos Epping <nikos.epping@protonmail.com>
2021-07-19 21:09:34 +0200

    Add log message to ignite script

--- ignite.sh
+++ ignite.sh
@@ -3,10 +3,9 @@
 set -e
 
 for butane_file in *.bu; do
-
-    echo ${butane_file}
-
     ignition_file=`basename "${butane_file}" .bu`.ign
+    echo "Igniting ${butane_file} -> ${ignition_file}"
+
     butane --pretty --strict < "${butane_file}" > "${ignition_file}"
 done
 
